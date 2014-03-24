790e43ab34a874f3a89d8f763a67d7631840f93f
Kate Taylor <kt6@sanger.ac.uk>
2014-03-24 15:05:32 +0000

    Start on adding gitver to all source files

---
+++ lib/test.pm
@@ -0,0 +1,13 @@
+#########
+# Author:        kt6
+# Maintainer:    $Author$
+# Created:       2009-04-17
+# Last Modified: $Date$
+# Id:            $Id$
+# $HeadURL$
+#
+
+package test;
+
+use Carp;
+1;
